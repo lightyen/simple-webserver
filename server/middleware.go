51b8574c17691eaea8847287f175c5af21e68052
lightyen <lightyen0123@gmail.com>
2020-08-07 23:31:43 +0800

    update

--- server/middleware.go
+++ server/middleware.go
@@ -2,14 +2,9 @@ package server
 
 import (
 	"log"
-	"path/filepath"
 	"runtime"
 	"strings"
-	"time"
 
-	"github.com/gin-contrib/cache"
-	"github.com/gin-contrib/cache/persistence"
-	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
@@ -35,17 +30,6 @@ func fallback(filename string, allowAny bool) gin.HandlerFunc {
 	}
 }
 
-func noCacheIndex() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		if c.Request.Method != "GET" {
-			return
-		}
-		if c.Request.URL.Path == "/" || c.Request.URL.Path == "/index.html" {
-			c.Header("Cache-Control", "no-store")
-		}
-	}
-}
-
 func recovery() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
@@ -60,17 +44,5 @@ func recovery() gin.HandlerFunc {
 			}
 		}()
 		c.Next()
-	}
-}
-
-func simpleHandles(bundleRoot string) []gin.HandlerFunc {
-	return []gin.HandlerFunc{
-		noCacheIndex(),
-		cache.CachePage(
-			persistence.NewInMemoryStore(5*time.Second),
-			time.Minute,
-			static.Serve("/", static.LocalFile(bundleRoot, false)),
-		),
-		fallback(filepath.Join(bundleRoot, "index.html"), true),
 	}
 }
--- server/router.go
+++ server/router.go
@@ -1,7 +1,13 @@
 package server
 
 import (
+	"path/filepath"
+	"time"
+
+	"github.com/gin-contrib/cache"
+	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-contrib/gzip"
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,6 +15,13 @@ func NewRouter(bundleRoot string) *gin.Engine {
 	e := gin.New()
 	e.Use(recovery())
 	e.Use(gzip.Gzip(gzip.DefaultCompression))
-	e.NoRoute(simpleHandles(bundleRoot)...)
+	indexCache := cache.CachePage(
+		persistence.NewInMemoryStore(0*time.Second),
+		0*time.Second,
+		static.Serve("/", static.LocalFile(bundleRoot, false)),
+	)
+	e.GET("/", indexCache)
+	e.GET("/index.html", indexCache)
+	e.NoRoute(static.Serve("/", static.LocalFile(bundleRoot, false)), fallback(filepath.Join(bundleRoot, "index.html"), true))
 	return e
 }
